3e72b4573f723119e30312d9bde8d0c8c91ecb0c
Georg <georgestoneix@gmail.com>
2024-07-08 12:08:43 -0700

    сделал CRUD

--- issuetracker/webapp/views.py
+++ issuetracker/webapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Issue
+from .forms import IssueForm
 
-# Create your views here.
+class IssueListView(ListView):
+    model = Issue
+    template_name = 'issue_list.html'
+
+class IssueDetailView(DetailView):
+    model = Issue
+    template_name = 'issue_detail.html'
+
+class IssueCreateView(CreateView):
+    model = Issue
+    form_class = IssueForm
+    template_name = 'issue_create.html'
+    success_url = reverse_lazy('issue_list')
+
+class IssueUpdateView(UpdateView):
+    model = Issue
+    form_class = IssueForm
+    template_name = 'issue_edit.html'
+    success_url = reverse_lazy('issue_list')
+
+class IssueDeleteView(DeleteView):
+    model = Issue
+    template_name = 'issue_delete.html'
+    success_url = reverse_lazy('issue_list')
