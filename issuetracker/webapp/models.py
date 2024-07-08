d1027455765fe5ac86ab01662303f5000922e351
Georg <georgestoneix@gmail.com>
2024-07-08 12:08:03 -0700

    создал модели задач, тип задач, и статусы.

--- issuetracker/webapp/models.py
+++ issuetracker/webapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    STATUS_CHOICES = [("new", "Новая"), ("moderated", "Модерированная"), ("deleted", "Удаленная")]
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "statuses"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+class Type(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "types"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.summary
+
+    class Meta:
+        db_table = "issues"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
