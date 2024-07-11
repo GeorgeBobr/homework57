65469dba8659bd01a654693d06dc6327098110a9
Georg <georgestoneix@gmail.com>
2024-07-11 06:56:10 -0700

    Обновил форму

--- issuetracker/webapp/forms.py
+++ issuetracker/webapp/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Issue
+from django.forms import widgets
 
-class IssueForm(forms.ModelForm):
-    class Meta:
-        model = Issue
-        fields = ['summary', 'description', 'status', 'type']
+from webapp.models import Type
+
+
+class IssueForm(forms.Form):
+    title = forms.CharField(max_length=3, required=True, label="Название")
+    author = forms.CharField(
+        max_length=50,
+        required=False,
+        label="Автор",
+        initial="Неизвестный",
+        widget=widgets.Input(attrs={"placeholder": "Автор"}),
+    )
+    content = forms.CharField(
+        max_length=3000,
+        required=True,
+        label="Контент",
+        widget=widgets.Textarea(attrs={"cols": 20, "rows": 5, "placeholder": "Контент"}),
+    )
+    types = forms.ModelChoiceField(queryset=Type.objects.all(), required=True)
