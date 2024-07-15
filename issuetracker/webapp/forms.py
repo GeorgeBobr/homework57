72f76219c8af249c5c22185b0a01e2ec02e9a2aa
Georg <georgestoneix@gmail.com>
2024-07-14 22:45:10 -0700

    Добавил кастомные валидаторы, также переделал формы

--- issuetracker/webapp/forms.py
+++ issuetracker/webapp/forms.py
@@ -1,18 +1,38 @@
 from django import forms
-from .models import Issue, Status, Type
+from django.forms import widgets
+from django.core.validators import BaseValidator
+from django.core.exceptions import ValidationError
 
-class IssueForm(forms.ModelForm):
-    class Meta:
-        model = Issue
-        fields = ['summary', 'description', 'status', 'types']
-        widgets = {
-            'summary': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
-            'types': forms.SelectMultiple(attrs={'class': 'form-control', 'size': 5}),
-        }
+from webapp.models import Type
+from webapp.models import Status
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['status'].queryset = Status.objects.all()
-        self.fields['types'].queryset = Type.objects.all()
+def validate_summary(value):
+    if "bad_word" in value.lower():
+        raise ValidationError("Заголовок содержит бранные слова")
+
+class NoSpecificCharactersValidator(BaseValidator):
+    message = "Заголовок не должен содержать определенные символы"
+    code = "no_specific_characters"
+
+    def compare(self, value, initial):
+        forbidden_characters = ['@', '#', '$', '^']
+        return not any(char in forbidden_characters for char in str(value))
+
+def at_least_5_summary(value):
+    if len(value) < 5:
+        raise ValidationError('Заголовок должен содержать не менее 5 символов')
+
+class IssueForm(forms.Form):
+    summary = forms.CharField(max_length=50, required=True, label="Заголовок",
+                              validators=[validate_summary, at_least_5_summary])
+    description = forms.CharField(max_length=2000, required=False, label="Описание",
+                                  widget=widgets.Textarea(attrs={"cols": 30, "rows": 5, "class": "test"}),)
+    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Типы")
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статусы")
+
+
+class TypeForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
+
+class StatusForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
