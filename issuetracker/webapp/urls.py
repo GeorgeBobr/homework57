bf2c2f26279e23bbfc50781bc42e64deb2c7e023
Georg <georgestoneix@gmail.com>
2024-07-14 22:43:53 -0700

    Немного переделал ссылки

--- issuetracker/webapp/urls.py
+++ issuetracker/webapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.views.generic import RedirectView
-from .views import IssueListView, IssueCreateView, IssueDetailView, IssueUpdateView, IssueDeleteView
+from .views import IssueListView, IssueDetailView, IssueCreateView, IssueUpdateView, IssueDeleteView
+
 
 urlpatterns = [
-    path('issues/', IssueListView.as_view(), name='index'),
-    path('', RedirectView.as_view(pattern_name='index')),
-    path('issue/create/', IssueCreateView.as_view(), name='issue_create'),
-    path('issue/<int:pk>/', IssueDetailView.as_view(), name='issue_detail'),
-    path('issue/<int:pk>/update/', IssueUpdateView.as_view(), name='issue_update'),
-    path('issue/<int:pk>/delete/', IssueDeleteView.as_view(), name='issue_delete'),
+    path('', IssueListView.as_view(), name='index'),
+    path('Issue/<int:id>/', IssueDetailView.as_view(), name='issue_detail'),
+    path('Issue/create/', IssueCreateView.as_view(), name='issue_create'),
+    path('Issue/update/<int:id>/', IssueUpdateView, name='issue_update'),
+    path('Issue/delete/<int:id>/', IssueDeleteView, name='issue_delete')
 ]
